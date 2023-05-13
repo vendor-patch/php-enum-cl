b2c0bb637eec59bc4a77402e50e46e9769ad334b
Till Wehowski <github@webfan.de>
2023-05-13 11:35:35 +0200

    Update webfan.boot.php

--- webfan.boot.php
+++ webfan.boot.php
@@ -6,18 +6,21 @@ if (PHP_VERSION_ID < 80000) {
     require_once __DIR__ . '/../php80/BackedEnum.php';
 }
 */
-( function($get74, $get80){   
+( function($get0, $get74, $get80){   
 
  $phpVersion = \PHP_VERSION_ID;
  $usePolytill = $phpVersion< 80000 ? true : false;
+ $slug = true === $usePolytill ? '74' : '80';
 
-
- $classMap =  match($usePolytill){
+ $Map =  match($usePolytill){
 		       true === $usePolytill    =>    $get74(),
 		       false === $usePolytill    =>    $get80(),
-			     default =>  $get74(),
+		       default =>  $get74(),
         }
 
+$classMap =  array_merge($get0($slug), $Map);	
+	
+	
 ( function($classMap){   
      \frdl\implementation\psr4\RemoteAutoloaderApiClient::getInstance('https://webfan.de/install/latest/?source={{class}}&salt={{salt}}', true)       
             ->withClassmap( $classMap );
@@ -29,6 +32,17 @@ if (PHP_VERSION_ID < 80000) {
   
   
    } )(
+
+  /*$get0*/   
+  function($slug){
+    return [
+	    'Mabe\Enum\Cl\\' => sprintf('https://raw.githubusercontent.com/vendor-patch/php-enum-cl/main/src/php%s/${class}.php?cache_bust=${salt}', $slug),
+	    'Mabe\Enum\Cl\\' => 'https://raw.githubusercontent.com/vendor-patch/php-enum-cl/main/src/psr-4/${class}.php?cache_bust=${salt}',
+	    \UnitEnum::class => 'https://raw.githubusercontent.com/vendor-patch/php-enum-cl/main/src/stubs/UnitEnum.php?cache_bust=${salt}',
+            \ValueError::class=>'https://raw.githubusercontent.com/vendor-patch/php-enum-cl/main/src/stubs/ValueError.php?cache_bust=${salt}',
+      ];
+  },	
+	
   /*$get74*/
   function(){
     return [
