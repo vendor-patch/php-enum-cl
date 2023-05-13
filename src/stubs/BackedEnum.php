882feafe97dbd8a35f145744874e6d0eca57d6e0
Till Wehowski <github@webfan.de>
2023-05-13 02:33:08 +0200

    Update BackedEnum.php

--- src/stubs/BackedEnum.php
+++ src/stubs/BackedEnum.php
@@ -1,7 +1,17 @@
 <?php declare(strict_types=1);
 
+
+/*
 if (PHP_VERSION_ID < 80000) {
     require_once __DIR__ . '/../php74/BackedEnum.php';
 } else {
     require_once __DIR__ . '/../php80/BackedEnum.php';
 }
+*/
+/*
+\Nette\PhpGenerator\EnumType::from(Abc\Enum1::class);
+  ClassType::from(new Abc\Class13);
+\Nette\PhpGenerator\ClassType::from(Abc\Trait13::class);
+$res[] = EnumType::from(Abc\Enum1::class);
+$res[] = EnumType::from(Abc\Enum2::class);
+*/
